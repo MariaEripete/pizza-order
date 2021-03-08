c5146fcec17f9f7ae4ad5b842a88cbfa90fe4427
MariaEripete <mariaichor@gmail.com>
2021-03-08 06:53:59 +0300

    change some functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
-$(".chosen-select").chosen({rtl: true}); 
+var choice= document.getElementById("choice").value;
+var crust= document.getElementById("crust").value;
+var quantity= document.getElementById("quantity").value;
+var location= document.getElementById("location").value;
 
-// the selector will match all input controls of type :checkbox
-// and attach a click event handler 
-$("input:checkbox").on('click', function() {
-    // in the handler, 'this' refers to the box clicked on
-    var $box = $(this);
-    if ($box.is(":checked")) {
-      // the name of the box is retrieved using the .attr() method
-      // as it is assumed and expected to be immutable
-      var group = "input:checkbox[name='" + $box.attr("name") + "']";
-      // the checked state of the group/box on the other hand will change
-      // and the current value is retrieved using .prop() method
-      $(group).prop("checked", false);
-      $box.prop("checked", true);
-    } else {
-      $box.prop("checked", false);
-    }
-});
+
+
+$ (document).ready(function(){
+    $('.multi-select').selectpicker();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//prototype
+function location() {
+    this.firstName = first;
+    this.lastName = last;
+  }
+  
+  Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
